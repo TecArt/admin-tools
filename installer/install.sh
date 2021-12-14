211c992051bfe2d6ef2bb7e4ece8d41301a4e6ca
izzy <360964+izzy@users.noreply.github.com>
2021-12-14 15:02:43 +0100

    Added workaround for systemd-resolved issues

--- installer/install.sh
+++ installer/install.sh
@@ -195,8 +195,16 @@ wget -O /usr/share/keyrings/sury-archive-keyring.gpg https://packages.sury.org/p
 
 apt-get update
 
+# Debian installs without systemd-resolved enabled but some packages require
+# it to be active. We'll back up the original resolv.conf for quick access.
+cp /etc/resolv.conf{,.dist}
+systemctl enable systemd-resolved.service
+
 echo "Installing dependencies. This might take a while..." >&3
 apt install -y tecart-archive-keyring tecart-essentials-server-5.2
+
+# Restore the resolv.conf in case the systemd resolver didn't work fast enough
+cp /etc/resolv.conf{.dist,}
 
 echo "Configuring timezone and locale" >&3
 echo "Europe/Berlin" > /etc/timezone
